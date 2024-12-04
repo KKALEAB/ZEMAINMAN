4431c6319abaf9e5cff445c0065f0cb4ba4a92d1
Kaleab Dessalegne <kaleabdess@gmail.com>
2024-12-03 23:27:28 -0500

    kebadsew hatrick serach

--- Ze.java
+++ Ze.java
@@ -2,5 +2,7 @@ public class Ze {
     public static void main(String[] args) {
         System.out.println("Hello Kebad Sew");
         System.out.println("Yidegem kebad sew");
+        System.out.println("ere third time kebad Yidegem kebad sew");
+
     }
 }
